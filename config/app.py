3c999408ccd1774d5061fdb9d257e3ff4a827198
Sydel Palinlin <sydel.palinlin@gmail.com>
2023-10-16 10:45:13 +0800

    First commit

---
+++ config/app.py
@@ -0,0 +1,26 @@
+import logging
+import os
+from functools import lru_cache
+
+from pydantic import AnyUrl, BaseSettings
+
+log = logging.getLogger("uvicorn")
+
+
+class Settings(BaseSettings):
+    ALLOWED_ORIGINS: str
+    SQS_PARSE_URL: str
+    APP_ENVIRONMENT: str
+
+    class Config:
+        env_file = ".env"
+        env_file_encoding = "utf-8"
+
+
+@lru_cache()
+def get_settings() -> BaseSettings:
+    log.info("Loading config settings from the environment...")
+    return Settings()
+
+
+settings = Settings()
---
+++ tests/test_main.py
@@ -0,0 +1,8 @@
+
+
+
+def test_main(http_event):
+
+    # Implement you test here...
+
+    assert True
