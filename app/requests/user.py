cad4992825aa1ce047992713e604f813c2e7ea7f
Sydel Palinlin <sydel.palinlin@gmail.com>
2023-10-19 08:09:42 +0800

    wip

--- app/requests/user.py
+++ app/requests/user.py
@@ -1,6 +1,12 @@
 from pydantic import BaseModel
 
+
 class UserCreate(BaseModel):
     username: str
     email: str
     password: str
+
+
+class UserEdit(UserCreate):
+    email: None
+    password: None
