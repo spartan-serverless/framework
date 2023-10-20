2beb1ab30f3eed85709c6c6d34168c8ad8f1b05d
Sydel Palinlin <sydel.palinlin@gmail.com>
2023-10-20 10:27:17 +0800

    wip

--- handlers/user.py
+++ handlers/user.py
@@ -1,4 +1,17 @@
+from app.services.user import UserService
+
+
 def main(event, context):
-    # Implement your code here...
+    user_service = UserService()
+
+    data = (
+        {
+            "username": "giana",
+            "email": "giana@artisan-sample.com",
+            "password": "sample333",
+        },
+    )
+
+    user_service.save(**data)
 
     return {"StatusCode": 200}
