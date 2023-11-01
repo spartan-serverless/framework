e915e4f823e9186c745959837d79fb501bf00a01
nerdmonkey <sydel.palinlin@gmail.com>
2023-10-31 21:37:01 -0400

    wip

--- config/database.py
+++ config/database.py
@@ -6,21 +6,22 @@ from sqlalchemy.orm import sessionmaker
 
 load_dotenv()
 
-host = os.environ.get("DB_HOST")
-port = os.environ.get("DB_PORT")
-database = os.environ.get("DB_NAME")
-username = os.environ.get("DB_USERNAME")
-password = os.environ.get("DB_PASSWORD")
+database = os.environ.get('DB_NAME')
 
-# database_url = f"postgresql+pg8000://{username}:{password}@{host}:{port}/{database}"
-# engine = create_engine(database_url)
+if os.environ.get("APP_ENVIRONMENT") == "test":
+    database_url = f"sqlite:///./database/{database}.db"
+else:
+    host = os.environ.get("DB_HOST")
+    port = os.environ.get("DB_PORT")
+    username = os.environ.get("DB_USERNAME")
+    password = os.environ.get("DB_PASSWORD")
 
-database_url = f"sqlite:///./database/artisan.db"
+    if os.environ.get("DB_TYPE") == "psql":
+        database_url = f"postgresql+pg8000://{username}:{password}@{host}:{port}/{database}"
+
 engine = create_engine(database_url)
 
-
 Session = sessionmaker(bind=engine)
-
 
 def get_session() -> Session:
     return Session()
