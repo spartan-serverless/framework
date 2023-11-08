7ced5a535c51786931dc222d39670163224cc70b
nerdmonkey <sydel.palinlin@gmail.com>
2023-11-08 06:32:45 -0500

    wip

--- database/migrations/env.py
+++ database/migrations/env.py
@@ -6,6 +6,11 @@ from sqlalchemy import engine_from_config, pool
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+section = config.config_ini_section
+config.set_section_option(section, "DB_USERNAME", "root")
+config.set_section_option(section, "DB_PASSWORD", "")
+config.set_section_option(section, "DB_HOST", "")
+config.set_section_option(section, "DB_NAME", "")
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
