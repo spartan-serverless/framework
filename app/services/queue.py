5e2cbe39ce76d8b457d0285bdbb3b360cafc5a75
nerdmonkey <sydel.palinlin@gmail.com>
2023-11-10 18:29:24 -0500

    wip

--- app/services/queue.py
+++ app/services/queue.py
@@ -1,32 +1,93 @@
 import json
-
 import boto3
-
+from typing import Any, Dict
 
 class QueueService:
-    def __init__(self) -> None:
-        self.sqs = boto3.client("sqs")
+    """
+    A service class to interact with AWS Simple Queue Service (SQS).
 
-    def send(
-        self, url: str, message: dict, group_id: int, deduplication_id: int
+    This class provides methods to send, receive, and delete messages from
+    an SQS queue. It uses the boto3 library to communicate with AWS services.
+    """
+
+    def __init__(self, region_name: str = 'us-east-1') -> None:
+        """
+        Initializes the QueueService class.
+
+        Creates an SQS client using boto3 which is used to interact with the AWS SQS.
+        """
+        self.sqs_client = boto3.client("sqs", region_name=region_name)
+
+    def _prepare_message(self, message: Dict[str, Any]) -> str:
+        """
+        Prepares a message for sending to the SQS queue by converting it to JSON.
+
+        Args:
+            message (Dict[str, Any]): The message to be sent.
+
+        Returns:
+            str: The JSON representation of the message.
+        """
+        return json.dumps(message)
+
+    def send_message(
+        self, queue_url: str, message: dict, group_id: str = None, deduplication_id: str = None
     ) -> dict:
-        response = self.sqs.send_message(
-            QueueUrl=url,
-            MessageBody=json.dumps(message),
-            MessageGroupId=str(group_id),
-            MessageDeduplicationId=str(deduplication_id),
+        """
+        Sends a message to an SQS queue.
+
+        This method automatically determines if the queue is a FIFO queue by
+        checking if the queue URL ends with '.fifo'. It adds the 'MessageGroupId'
+        and 'MessageDeduplicationId' parameters only if it's a FIFO queue.
+
+        Args:
+            queue_url (str): The URL of the SQS queue.
+            message (dict): The message to send.
+            group_id (str, optional): The group ID of the message, used for FIFO queues.
+            deduplication_id (str, optional): The deduplication ID of the message, used for FIFO queues.
+
+        Returns:
+            dict: The response from the SQS service after sending the message.
+        """
+        send_params = {
+            "QueueUrl": queue_url,
+            "MessageBody": json.dumps(message)
+        }
+
+        # Check if the queue is a FIFO queue
+        if queue_url.endswith('.fifo'):
+            if group_id is not None:
+                send_params["MessageGroupId"] = group_id
+            if deduplication_id is not None:
+                send_params["MessageDeduplicationId"] = deduplication_id
+
+        return self.sqs_client.send_message(**send_params)
+
+
+
+    def receive_message(self, queue_url: str) -> Dict[str, Any]:
+        """
+        Receives a message from an SQS queue.
+
+        Args:
+            queue_url (str): The URL of the SQS queue.
+
+        Returns:
+            Dict[str, Any]: The response from the SQS service containing the message(s) received.
+        """
+        return self.sqs_client.receive_message(
+            QueueUrl=queue_url, AttributeNames=["All"], MessageAttributeNames=["All"]
         )
 
-        return response
+    def delete_message(self, queue_url: str, receipt_handle: str) -> dict:
+        """
+        Deletes a message from an SQS queue.
 
-    def receive(self, url: str):
-        response = self.sqs.receive_message(
-            QueueUrl=url, AttributeNames=["All"], MessageAttributeNames=["All"]
-        )
+        Args:
+            queue_url (str): The URL of the SQS queue.
+            receipt_handle (str): The receipt handle of the message to be deleted.
 
-        return response
-
-    def delete(self, url: str, receipt_handle: str):
-        response = self.sqs.delete_message(QueueUrl=url, ReceiptHandle=receipt_handle)
-
-        return response
+        Returns:
+            dict: The response from the SQS service after deleting the message.
+        """
+        return self.sqs_client.delete_message(QueueUrl=queue_url, ReceiptHandle=receipt_handle)
--- tests/conftest.py
+++ tests/conftest.py
@@ -5,4 +5,15 @@ from public.main import app
 
 @pytest.fixture
 def client():
+    """
+    A pytest fixture that provides a test client for the Spartan app.
+
+    This fixture creates a TestClient instance using the Spartan application
+    imported from public.main. It's used for testing the API endpoints in the
+    application. The fixture can be used in test functions to make requests to
+    the API and assert the responses.
+
+    Returns:
+        TestClient: An instance of TestClient wrapped around the Spartan app.
+    """
     return TestClient(app)
