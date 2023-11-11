e1ab41918ab48c31c2511fbff030d7cee8afedbd
nerdmonkey <sydel.palinlin@gmail.com>
2023-11-10 23:41:24 -0500

    wip

--- app/models/user.py
+++ app/models/user.py
@@ -1,9 +1,17 @@
 from sqlalchemy import Column, Integer, String
-
 from .base import Base
 
-
 class User(Base):
+    """
+    Database model for User.
+
+    Attributes:
+        id (int): The primary key for the User table.
+        username (str): The unique username of the user.
+        email (str): The unique email address of the user.
+        password (str): The hashed password of the user.
+    """
+
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
