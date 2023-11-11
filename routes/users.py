0f8e36087977f72e3bf25b1e75fc0f9ae214c092
nerdmonkey <sydel.palinlin@gmail.com>
2023-11-11 14:45:19 -0500

    wip

--- routes/users.py
+++ routes/users.py
@@ -1,7 +1,6 @@
 from typing import List, Optional
 from fastapi import APIRouter, Depends, HTTPException, Query
 from sqlalchemy.orm import Session
-
 from app.models.user import User
 from app.requests.user import UserCreateRequest, UserUpdateRequest
 from app.responses.user import UserCreateResponse, UserResponse, UserUpdateResponse
@@ -40,68 +39,44 @@ async def get_users(
     db: Session = Depends(get_session),
 ):
     try:
-        # Calculate the offset based on page and items_per_page
         offset = (page - 1) * items_per_page
-
-        # Query the database with pagination
         users = db.query(User).offset(offset).limit(items_per_page).all()
-
-        # Create a list of UserResponse objects from the database results
         user_responses = [
             UserResponse(id=user.id, username=user.username, email=user.email)
             for user in users
         ]
-
         return user_responses
-
     except DatabaseError as e:
-        # Log the exception details, e.g., using logging
-        # logging.error(f"Database error occurred: {e}")
-        raise HTTPException(
-            status_code=500, internal_server_error="Internal server error"
-        )
-
+        raise HTTPException(status_code=500, detail="Internal server error")
     except Exception as e:
-        # Handle unexpected exceptions
-        # logging.error(f"Unexpected error occurred: {e}")
-        raise HTTPException(
-            status_code=500, internal_server_error="Internal server error"
-        )
+        raise HTTPException(status_code=500, detail="Internal server error")
 
 
 @route.get("/users/{user_id}", status_code=200, response_model=UserResponse)
 async def get_user(user_id: int, db: Session = Depends(get_session)):
     user = db.query(User).filter(User.id == user_id).first()
-
     if not user:
         raise HTTPException(status_code=404, detail="User not found")
-
     return UserResponse(id=user.id, username=user.username, email=user.email)
 
 
 @route.post("/users", status_code=201, response_model=UserCreateResponse)
 async def create_user(user: UserCreateRequest, db: Session = Depends(get_session)):
-    # Check if a user with the same email already exists
     existing_user = db.query(User).filter(User.email == user.email).first()
     if existing_user:
         raise HTTPException(
             status_code=422, detail="User with this email already exists"
         )
-
-    # TODO: Add password hashing logic here
     hashed_password = "hashed_" + user.password
     db_user = User(username=user.username, email=user.email, password=hashed_password)
     db.add(db_user)
     db.commit()
     db.refresh(db_user)
-
-    # Construct a dictionary that matches the UserCreateResponse model
     response_data = {
         "id": db_user.id,
         "username": db_user.username,
         "email": db_user.email,
     }
-
     return response_data
 
 
@@ -111,21 +86,17 @@ async def update_user(
 ):
     db_user = get_user_by_id(user_id, db)
     user_data = user.dict(exclude_unset=True)
-    # TODO: Add password hashing logic here if password is being updated
     if "password" in user_data:
         user_data["password"] = "hashed_" + user_data["password"]
     for key, value in user_data.items():
         setattr(db_user, key, value)
     db.commit()
     db.refresh(db_user)
-
-    # Construct a dictionary that matches the UserUpdateResponse model
     response_data = {
         "id": db_user.id,
         "username": db_user.username,
         "email": db_user.email,
     }
-
     return response_data
 
 
