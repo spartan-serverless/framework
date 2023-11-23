2b557037ec177fbb82d85fe6fc6533ad55073bb5
Sydel Palinlin <sydel.palinlin@gmail.com>
2023-11-23 14:05:50 -0500

    wip

--- database/seeders/user_seeder.py
+++ database/seeders/user_seeder.py
@@ -3,43 +3,18 @@ from app.models.user import User
 
 def run():
     users = [
-        {
-            "username": "andeng",
-            "email": "andeng@python-spartan.com",
-            "password": "andeng",
-        },
-        {
-            "username": "iviang",
-            "email": "iviang@python-spartan.com",
-            "password": "iviang",
-        },
+        {"username": "andeng", "email": "andeng@python-spartan.com", "password": "andeng", },
+        {"username": "iviang", "email": "iviang@python-spartan.com", "password": "iviang", },
         {"username": "loki", "email": "loki@python-spartan.com", "password": "loki"},
         {"username": "laleng", "email": "laleng@python-spartan.com", "password": "laleng"},
         {"username": "zeus", "email": "zeus@python-spartan.com", "password": "zeus123"},
         {"username": "hera", "email": "hera@python-spartan.com", "password": "hera123"},
-        {
-            "username": "athena",
-            "email": "athena@python-spartan.com",
-            "password": "athena123",
-        },
-        {
-            "username": "apollo",
-            "email": "apollo@python-spartan.com",
-            "password": "apollo123",
-        },
-        {
-            "username": "artemis",
-            "email": "artemis@python-spartan.com",
-            "password": "artemis123",
-        },
-        {
-            "username": "hunk",
-            "email": "hunk@python-spartan.com",
-            "password": "hunk123",
-        },
+        {"username": "athena", "email": "athena@python-spartan.com", "password": "athena123", },
+        {"username": "apollo", "email": "apollo@python-spartan.com", "password": "apollo123", },
+        {"username": "artemis", "email": "artemis@python-spartan.com", "password": "artemis123", },
+        {"username": "hunk", "email": "hunk@python-spartan.com", "password": "hunk123", },
     ]
 
-    user_model = User
     db = get_session()
 
     for user in users:
