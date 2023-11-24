68d38ff8f42168e61081d77fcdc62726fb6d70f1
Sydel Palinlin <sydel.palinlin@gmail.com>
2023-11-24 04:01:40 -0500

    wip

--- app/requests/user.py
+++ app/requests/user.py
@@ -10,28 +10,8 @@ class UserCreateRequest(BaseModel):
     email: EmailStr
     password: str
 
-    @validator('email')
-    def email_must_be_unique(cls, v):
-        if cls.is_email_exist(v, db):
-            raise ValueError('Email already in use')
-        return v
-
-    def is_email_exist(email: str, session: db) -> bool:
-        return session.query(User).filter(User.email == email).first() is not None
 
 class UserUpdateRequest(BaseModel):
     username: Optional[str]
     email: Optional[EmailStr]
     password: Optional[str]
-
-    @classmethod
-    def is_email_exist(cls, email: str, user_id: int, db: db) -> bool:
-        return db.query(User).filter(User.email == email, User.id != user_id).first() is not None
-
-    @validator('email')
-    def email_must_be_unique(cls, v, values, **kwargs):
-        db = kwargs.get('db')
-        user_id = kwargs.get('user_id')
-        if v and db and user_id and cls.is_email_exist(v, user_id, db):
-            raise ValueError('Email already in use')
-        return v
