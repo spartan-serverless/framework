e404aeb4c5503ebe23a1ec1a6dfa9801c8186fe4
Sydel Palinlin <sydel.palinlin@gmail.com>
2023-11-23 20:46:47 -0500

    wip

--- app/requests/user.py
+++ app/requests/user.py
@@ -1,31 +1,29 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, validator, EmailStr
+from typing import Optional
+from app.models.user import User
+from config.database import get_session
 
+db = get_session()
 
 class UserCreateRequest(BaseModel):
-    """
-    Pydantic model representing a request for creating a User.
-
-    Attributes:
-        username (str): The username of the user.
-        email (str): The email address of the user.
-        password (str): The password of the user.
-    """
-
     username: str
-    email: str
+    email: EmailStr
     password: str
 
+    @validator('email')
+    def email_must_be_unique(cls, v):
+        if cls.is_email_exist(v, db):
+            raise ValueError('Email already in use')
+        return v
+
+    def is_email_exist(email: str, session: db) -> bool:
+        return session.query(User).filter(User.email == email).first() is not None
 
 class UserUpdateRequest(UserCreateRequest):
-    """
-    Pydantic model representing a request for updating a User.
-
-    Attributes:
-        username (str): The updated username of the user.
-        email (str): The updated email address of the user.
-        password (str): The updated password of the user.
-    """
-
-    username: str
-    email: str
-    password: str
+    @validator('email')
+    def email_must_be_unique(cls, v, values, **kwargs):
+        # Assuming you have access to the user's ID or some unique identifier here
+        user_id = values.get('user_id')  # Replace 'user_id' with the actual field name
+        if cls.is_email_exist(v, db):
+            raise ValueError('Email already in use')
+        return v
