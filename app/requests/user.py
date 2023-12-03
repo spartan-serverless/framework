4a492c2ae6055c4e3c5632b2bec9aace5c9cc47e
Sydel Palinlin <sydel.palinlin@gmail.com>
2023-12-03 11:09:17 +0800

    wip

--- app/requests/user.py
+++ app/requests/user.py
@@ -1,20 +1,33 @@
 from typing import Optional
 
-from pydantic import BaseModel, EmailStr, validator
-
-from app.models.user import User
-from config.database import get_session
-
-db = get_session()
+from pydantic import BaseModel, EmailStr
 
 
 class UserCreateRequest(BaseModel):
+    """
+    Data model for creating a new user.
+
+    Attributes:
+        username (str): The username of the new user.
+        email (EmailStr): The email address of the new user.
+        password (str): The password for the new user.
+    """
+
     username: str
     email: EmailStr
     password: str
 
 
 class UserUpdateRequest(BaseModel):
+    """
+    Data model for updating an existing user.
+
+    Attributes:
+        username (Optional[str]): The new username of the user. Optional.
+        email (Optional[EmailStr]): The new email address of the user. Optional.
+        password (Optional[str]): The new password for the user. Optional.
+    """
+
     username: Optional[str]
     email: Optional[EmailStr]
     password: Optional[str]
