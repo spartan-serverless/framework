30a0422037dcec2469a29e7d297a0aca0f94a017
Sydel Palinlin <sydel.palinlin@gmail.com>
2023-12-09 16:20:40 +0800

    wip

--- tests/feature/test_users_endpoints.py
+++ tests/feature/test_users_endpoints.py
@@ -3,75 +3,58 @@ from fastapi import status
 
 # Test case for creating a user
 def test_create_user(client):
-    # Given: the payload for a new user
     payload = {
         "username": "testuseranother",
         "email": "testuseranother@example.com",
         "password": "test1password",
     }
-
-    # When: the client makes a POST request to create a user
     response = client.post("/api/users", json=payload)
-
-    # Then: the server should respond with a 201 status code
-    # and the response body should contain the username of the new user
     assert response.status_code == status.HTTP_201_CREATED
-    assert response.json()["username"] == "testuseranother"
+    data = response.json()
+    assert data['data']["username"] == "testuseranother"
+    assert "email" in data['data']  # Verify that the email is also returned
+
 
 
 # Test case for retrieving a list of users
 def test_read_users(client):
-    # Given: an existing set of users
-
-    # When: the client makes a GET request to fetch the list of users
     response = client.get("/api/users")
-
-    # Then: the server should respond with a 200 status code
-    # and the response body should be a list
     assert response.status_code == status.HTTP_200_OK
-    assert isinstance(response.json(), list)
+    data = response.json()
+    assert isinstance(data['data'], list)  # Confirm that it's a list
+    assert all("username" in user for user in data['data'])  # Check each user has a username
+
 
 
 # Test case for retrieving a single user
 def test_read_user(client):
-    # Given: a user with a known ID
-    user_id = 1  # Replace with a setup to create a user and get the ID
-
-    # When: the client makes a GET request to fetch the user details
+    user_id = 1  # This should be replaced with a dynamic user creation and fetching of its ID
     response = client.get(f"/api/users/{user_id}")
-
-    # Then: the server should respond with a 200 status code
-    # and the response body should contain the details of the user
     assert response.status_code == status.HTTP_200_OK
-    assert response.json()["id"] == user_id
+    data = response.json()
+    assert data['data']["id"] == user_id
+    assert "username" in data['data']  # Verify that the username is in the response
+
 
 
 # Test case for updating a user's information
-def test_update_user(client, test_data):
-    # Given: a user with a known ID and the payload for updating the user
-    user_id = 1  # Replace with a setup to create a user and get the ID
+def test_update_user(client):
+    user_id = 1  # Replace with dynamic user creation
     payload = {
         "username": "updateduser",
         "email": "updateduser@example.com",
         "password": "updatedpassword",
     }
-
-    # When: the client makes a PUT request to update the user's information
     response = client.put(f"/api/users/{user_id}", json=payload)
-
-    # Then: the server should respond with a 200 status code
-    # and the response body should reflect the updated user information
     assert response.status_code == status.HTTP_200_OK
-    assert response.json()["username"] == "updateduser"
+    data = response.json()
+    assert data["data"]["username"] == "updateduser"
+    assert "email" in data["data"]  # Check for email in the response
+
 
 
 # Test case for deleting a user
 def test_delete_user(client):
-    # Given: a user with a known ID
-    user_id = 1  # Replace with a setup to create a user and ensure they exist
-
-    # When: the client makes a DELETE request to remove the user
+    user_id = 1  # Replace with dynamic user creation
     response = client.delete(f"/api/users/{user_id}")
-    print(response.content)
-    # Then: the server should respond with a 204 status code indicating successful deletion
-    assert response.status_code == status.HTTP_204_NO_CONTENT
+    assert response.status_code == status.HTTP_200_OK
--- tests/unit/test_user_service.py
+++ tests/unit/test_user_service.py
@@ -7,7 +7,6 @@ from app.requests.user import UserCreateRequest, UserUpdateRequest
 from app.responses.user import UserCreateResponse, UserResponse, UserUpdateResponse
 from app.services.user import UserService
 
-
 # Create a mock database session for testing
 @pytest.fixture
 def mock_db_session():
@@ -19,19 +18,38 @@ def mock_db_session():
             return self
 
         def filter(self, condition):
+            self.query_condition = condition.right.value  # Assuming condition is User.id == id
+            return self
+
+
+        def filter(self, condition):
+            # Assume the condition is structured like User.email == email
+            attribute = condition.left.key
+            value = condition.right.value
+
+            self.filtered_users = [user for user in self.users if getattr(user, attribute) == value]
             return self
 
         def first(self):
-            return self.users[0] if self.users else None
+            return self.filtered_users[0] if self.filtered_users else None
 
-        def all(self):
-            return self.users
 
         def offset(self, offset):
+            self.pagination_offset = offset
             return self
 
         def limit(self, limit):
+            self.pagination_limit = limit
             return self
+
+        def all(self):
+            start = self.pagination_offset
+            end = start + self.pagination_limit
+            return self.users[start:end]
+
+
+        def count(self):
+            return len(self.users)
 
         def add(self, item):
             self.users.append(item)
@@ -47,118 +65,106 @@ def mock_db_session():
 
     return MockSession()
 
+# Helper function to add users to mock_db_session
+def add_users_to_session(session, num_users):
+    for i in range(1, num_users + 1):
+        user = User(id=i, username=f"user{i}", email=f"user{i}@example.com")
+        session.users.append(user)
 
 # Tests for UserService
-def test_get_by_id(mock_db_session):
+@pytest.mark.parametrize("user_id, expected_result", [(1, True), (99, False)])
+def test_get_by_id(mock_db_session, user_id, expected_result):
+    add_users_to_session(mock_db_session, 5)
+    user_service = UserService(db=mock_db_session)
+
+    if expected_result:
+        result = user_service.get_by_id(user_id)
+        assert result.id == user_id
+    else:
+        with pytest.raises(HTTPException) as exc_info:
+            user_service.get_by_id(user_id)
+        assert exc_info.value.status_code == 404
+
+def test_all_with_pagination(mock_db_session):
+    add_users_to_session(mock_db_session, 10)
+    user_service = UserService(db=mock_db_session)
+
+    result, total = user_service.all(page=2, items_per_page=5)
+
+    assert len(result) == 5
+    assert total == 10
+
+def test_total(mock_db_session):
+    add_users_to_session(mock_db_session, 3)
+    user_service = UserService(db=mock_db_session)
+
+    result = user_service.total()
+
+    assert result == 3
+
+def test_find_user(mock_db_session):
+    # Adding a user to the mock session
     user1 = User(id=1, username="user1", email="user1@example.com")
     mock_db_session.users.append(user1)
+
     user_service = UserService(db=mock_db_session)
 
-    result = user_service.get_by_id(1)
-
-    assert result == user1
-
-
-def test_get_by_id_not_found(mock_db_session):
-    user_service = UserService(db=mock_db_session)
-
-    with pytest.raises(HTTPException):
-        user_service.get_by_id(1)
-
-
-def test_all(mock_db_session):
-    user1 = User(id=1, username="user1", email="user1@example.com")
-    user2 = User(id=2, username="user2", email="user2@example.com")
-    mock_db_session.users.extend([user1, user2])
-    user_service = UserService(db=mock_db_session)
-
-    result = user_service.all(page=1, items_per_page=2)
-
-    assert len(result) == 2
-    assert result[0].id == user1.id
-    assert result[1].id == user2.id
-
-
-def test_get(mock_db_session):
-    user1 = User(id=1, username="user1", email="user1@example.com")
-    mock_db_session.users.append(user1)
-    user_service = UserService(db=mock_db_session)
-
+    # Finding the user by id
     result = user_service.find(1)
 
-    assert result.id == user1.id
+    # Assertions
+    assert result.id == 1
+    assert result.username == "user1"
 
 
-def test_get_not_found(mock_db_session):
+def test_save_user(mock_db_session):
     user_service = UserService(db=mock_db_session)
+    user_request = UserCreateRequest(username="new_user", email="new_user@example.com", password="password")
 
-    with pytest.raises(HTTPException):
-        user_service.find(1)
+    result = user_service.save(user_request)
 
+    assert result["username"] == user_request.username
+    assert result["email"] == user_request.email
+    assert len(mock_db_session.users) == 1
 
-def test_create(mock_db_session):
-    user_create_request = UserCreateRequest(
-        username="new", email="new@example.com", password="password"
-    )
+def test_save_duplicate_email_user(mock_db_session):
+    add_users_to_session(mock_db_session, 1)
     user_service = UserService(db=mock_db_session)
+    user_request = UserCreateRequest(username="user2", email="user1@example.com", password="password")
 
-    result = user_service.save(user_create_request)
+    with pytest.raises(HTTPException) as exc_info:
+        user_service.save(user_request)
+    assert exc_info.value.status_code == 422
 
-    assert result is not None
-    assert "id" in result
-    assert result["username"] == user_create_request.username
-    assert result["email"] == user_create_request.email
-
-
-def test_create_duplicate_email(mock_db_session):
-    user1 = User(id=1, username="user1", email="user1@example.com")
-    user_create_request = UserCreateRequest(
-        username="user1", email="user1@example.com", password="password"
-    )
-    mock_db_session.users.append(user1)
+def test_update_user(mock_db_session):
+    add_users_to_session(mock_db_session, 1)
     user_service = UserService(db=mock_db_session)
+    user_request = UserUpdateRequest(username="updated_user", email="updated_user@example.com")
 
-    with pytest.raises(HTTPException):
-        user_service.save(user_create_request)
+    result = user_service.update(1, user_request)
 
+    assert result["username"] == user_request.username
+    assert result["email"] == user_request.email
 
-def test_update(mock_db_session):
-    user1 = User(id=1, username="user1", email="user1@example.com")
-    user_update_request = UserUpdateRequest(
-        username="updated", email="updated@example.com", password="new_password"
-    )
-    mock_db_session.users.append(user1)
+def test_update_nonexistent_user(mock_db_session):
     user_service = UserService(db=mock_db_session)
+    user_request = UserUpdateRequest(username="user2", email="user2@example.com")
 
-    result = user_service.update(1, user_update_request)
+    with pytest.raises(HTTPException) as exc_info:
+        user_service.update(2, user_request)
+    assert exc_info.value.status_code == 404
 
-    assert result["id"] == user1.id
-    assert result["username"] == user_update_request.username
-    assert result["email"] == user_update_request.email
-
-
-def test_update_not_found(mock_db_session):
-    user_update_request = UserUpdateRequest(
-        username="updated", email="updated@example.com", password="new_password"
-    )
-    user_service = UserService(db=mock_db_session)
-
-    with pytest.raises(HTTPException):
-        user_service.update(1, user_update_request)
-
-
-def test_delete(mock_db_session):
-    user1 = User(id=1, username="user1", email="user1@example.com")
-    mock_db_session.users.append(user1)
+def test_delete_user(mock_db_session):
+    add_users_to_session(mock_db_session, 1)
     user_service = UserService(db=mock_db_session)
 
     user_service.delete(1)
 
     assert len(mock_db_session.users) == 0
 
-
-def test_delete_not_found(mock_db_session):
+def test_delete_nonexistent_user(mock_db_session):
     user_service = UserService(db=mock_db_session)
 
-    with pytest.raises(HTTPException):
-        user_service.delete(1)
+    with pytest.raises(HTTPException) as exc_info:
+        user_service.delete(2)
+    assert exc_info.value.status_code == 404
