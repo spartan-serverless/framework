6955e930de8270f4e461fa9f293916e6a24dac0c
Sydel Palinlin <sydel.palinlin@gmail.com>
2023-12-17 11:16:58 +0800

    wip

--- database/seeders/user_seeder.py
+++ database/seeders/user_seeder.py
@@ -11,12 +11,15 @@ def run():
 
     current_time = datetime.now()
     two_months_ago = current_time - timedelta(days=60)
+    one_week_ago = current_time - timedelta(days=7)
 
-    for x in range(0, 30):
+    for x in range(0, 60):
         user = {
             "username": fake.user_name(),
             "email": fake.email(),
             "password": fake.password(),
+            "created_at": fake.date_time_between(start_date=two_months_ago, end_date=one_week_ago),
+            "updated_at": fake.date_time_between(start_date=one_week_ago, end_date=current_time),
         }
         user = User(**user)
         db.add(user)
